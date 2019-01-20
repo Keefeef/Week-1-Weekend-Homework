ecaf982dbf78b74c3ae9f7a762666726d6d79f19
Ethan Radcliffe <ethanr@mac.com>
2019-01-20 20:18:49 +0000

    11 Tests completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -2,14 +2,19 @@ def pet_shop_name(name)
   return name[:name]
 end
 
+
 def total_cash(cash)
   return cash[:admin][:total_cash]
 end
 
-def add_or_remove_cash(cash, total, total_cash)
-  total = 10
-total_cash = total += cash[:admin][:total_cash]
-    return total_cash
+
+def add_or_remove_cash(cash, total)
+  return cash[:admin][:total_cash] += total
+end
+
+
+def add_or_remove_cash__remove(cash, total)
+    return cash[:admin][:total_cash] -= total
   end
 
 
@@ -17,15 +22,44 @@ def pets_sold(pets)
   return pets[:admin][:pets_sold]
 end
 
-# def stock_count(pets)
-#   total_stock = 0
-#   for pet in pets
-#     total_stock += pet[:pets][:name].to_i
+
+def increase_pets_sold(petshop, petsalenum)
+  return petshop[:admin][:pets_sold] += petsalenum
+end
+
+
+def stock_count(pets)
+return pets[:pets].length
+end
+
+
+def pets_by_breed(petshop,search)
+  result = []
+  for pet in petshop[:pets]
+    result.push(pet[:breed]) if (pet[:breed] == search)
+  end
+  return result
+end
+
+# def pets_by_breed__found(petshop,search)
+#   for pet in petshop[:pets]
+#     if (pet[:breed] != search)
+#     end
+#     return 0
 #   end
-#   return total_stock
-# end
 
 
-# def customer_cash(cash)
-#   return cash customers[:cash]
-# end
+def find_pet_by_name(petshop,name)
+  for pet in petshop[:pets]
+    return pet if (pet[:name] == name)
+  end
+  return nil
+end
+
+def remove_pet_by_name(petshop,name)
+  loopcounter = 0
+  for pet in petshop[:pets]
+    petshop[:pets].delete_at(loopcounter) if (pet[:name] == name)
+    loopcounter += 1
+  end
+end
